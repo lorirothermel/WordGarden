f64d3f9ddaf1c00154a26f8ba257eaa5f7b92840
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-03-24 01:33:20 -0400

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Lori Rothermel on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
